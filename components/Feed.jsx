76135afa2e6e1bed1e2b9698498927f8a1383abd
meshJohn <meshjohn1@gmail.com>
2024-08-07 09:30:32 +0300

    fuck

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
-import useSWR, { mutate } from "swr";
+import { useState, useCallback } from "react";
+import useSWR from "swr";
 import PromptCard from "./PromptCard";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -21,47 +21,37 @@ const PromptCardList = ({ data, handleTagClick }) => {
 };
 
 const Feed = () => {
-  const { data: allPosts, error } = useSWR("/api/prompt", fetcher);
+  const { data: allPosts, error, mutate } = useSWR('/api/prompt', fetcher);
   const [searchText, setSearchText] = useState("");
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState([]);
 
-  const filterPrompts = useCallback(
-    (searchText) => {
-      const regex = new RegExp(searchText, "i");
-      return allPosts.filter(
-        (item) =>
-          regex.test(item.creator.username) ||
-          regex.test(item.tag) ||
-          regex.test(item.prompt)
-      );
-    },
-    [allPosts]
-  );
+  const filterPrompts = (searchText) => {
+    const regex = new RegExp(searchText, "i");
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
 
   const handleSearchChange = (e) => {
     clearTimeout(searchTimeout);
     setSearchText(e.target.value);
     setSearchTimeout(
       setTimeout(() => {
-        if (allPosts) {
-          const searchResults = filterPrompts(e.target.value);
-          setSearchedResults(searchResults);
-        }
+        const searchResults = filterPrompts(e.target.value);
+        setSearchedResults(searchResults);
       }, 500)
     );
   };
 
   const handleTagClick = (tagName) => {
     setSearchText(tagName);
-    if (allPosts) {
-      const searchResults = filterPrompts(tagName);
-      setSearchedResults(searchResults);
-    }
+    const searchResults = filterPrompts(tagName);
+    setSearchedResults(searchResults);
   };
-
-  if (error) return <div>Failed to load</div>;
-  if (!allPosts) return <div>Loading...</div>;
 
   return (
     <section className="feed">
